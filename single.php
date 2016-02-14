b65472b18d38c4022884ee8dacf38efbc212eee2
lyndachiwetelu <lyndachiwe@gmail.com>
2016-02-14 19:24:53 +0100

    Added comment form

--- single.php
+++ single.php
@@ -124,6 +124,45 @@
 					</div>
 					<div class="row">
 						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+						<?php $args = array(
+	'author_email' => '',
+	'author__in' => '',
+	'author__not_in' => '',
+	'include_unapproved' => '',
+	'fields' => '',
+	'ID' => '',
+	'comment__in' => '',
+	'comment__not_in' => '',
+	'karma' => '',
+	'number' => '',
+	'offset' => '',
+	'orderby' => '',
+	'order' => 'DESC',
+	'parent' => '',
+	'post_author__in' => '',
+	'post_author__not_in' => '',
+	'post_ID' => '', // ignored (use post_id instead)
+	'post_id' => 0,
+	'post__in' => '',
+	'post__not_in' => '',
+	'post_author' => '',
+	'post_name' => '',
+	'post_parent' => '',
+	'post_status' => '',
+	'post_type' => '',
+	'status' => 'all',
+	'type' => '',
+        'type__in' => '',
+        'type__not_in' => '',
+	'user_id' => '',
+	'search' => '',
+	'count' => false,
+	'meta_key' => '',
+	'meta_value' => '',
+	'meta_query' => '',
+	'date_query' => null, // See WP_Date_Query
+);
+get_comments( $args ); ?>
 						<?php comment_form(); ?></div>
 					</div>
 
