7e14365423de91530576650406c757f24a1159e3
lyndachiwetelu <lyndachiwe@gmail.com>
2016-02-21 13:08:59 +0100

    style changes for footer and content page redesign

--- footer.php
+++ footer.php
@@ -1,60 +1,65 @@
 <footer class="footer">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"> <p>POPULAR ON MY BLOG</p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"> 
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"> <p>POPULAR ON MY BLOG</p>
 
 				<?php query_posts(array('orderby' => 'comment_count', 'posts_per_page' => '4'));
-if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-					
-					<p><?php the_title(); ?> <a href="<?php the_permalink(); ?>"><span class="ash"><?php comments_number('0','%','%'); ?></span></a></p>
-					
-<?php endwhile; endif;?>
-					
 
+				<p><?php the_title(); ?> <a href="<?php the_permalink(); ?>"><span class="ash"><?php comments_number('0','%','%'); ?></span></a></p>
+
+			<?php endwhile; endif;?>
+
+			<hr/>
+
+			<p>WANT TO GET NOTIFIED OF MY NEXT POST?</p>
+
+			<form class="navbar-form" role="search" method="get">
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Email Address" >
+					<span class="input-group-btn">
+						<button class="btn btn-primary whiteonblack" type="submit">Yes!</button>
+					</span>
 				</div>
+			</form>
 
-				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-				<?php get_search_form(); ?>
-
-				<p>TAG CLOUD</p>
-
-				<?php 
-$args  = array('number' => 10,'orderby'=>'count' );
-$tags = get_tags();
-//$html = '<div class="post_tags">';
-foreach ( $tags as $tag ) {
-	$tag_link = get_tag_link( $tag->term_id ); 
-	$tag_name = $tag->name; ?>
-
-
-					<a href="<?php echo $tag_link; ?>" title="<?php echo $tag_name; ?>"><span class="ash"><?php echo $tag_name; ?></span></a>
-			
-<?php	
-}
-
-
-?>
-
-
-				</div>
-				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"> <p>WANT TO GET NOTIFIED OF MY NEXT POST?</p>
-
-					<form class="navbar-form" role="search" method="get">
-<div class="input-group">
-						<input type="text" class="form-control" placeholder="Email Address" >
-						<span class="input-group-btn">
-							<button class="btn btn-primary whiteonblack" type="submit">Yes!</button>
-						</span>
-					</div>
-</form>
-					
-
-				</div>
-
-			</div>
 		</div>
-	</footer>
+
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+			<?php get_search_form(); ?>
+
+			<p>TAG CLOUD</p>
+
+			<?php 
+			$args  = array('number' => 10,'orderby'=>'count' );
+			$tags = get_tags();
+//$html = '<div class="post_tags">';
+			foreach ( $tags as $tag ) {
+				$tag_link = get_tag_link( $tag->term_id ); 
+				$tag_name = $tag->name; ?>
+
+
+				<a href="<?php echo $tag_link; ?>" title="<?php echo $tag_name; ?>"><span class="ash"><?php echo $tag_name; ?></span></a>
+
+				<?php	
+			}
+
+
+			?>
+
+
+		</div>
+
+		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"> 
+		</div>
+
+	</div>
+</div>
+</footer>
 
 
 	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
--- single.php
+++ single.php
@@ -2,65 +2,90 @@
 
 <div class="container-fluid">
 	<div class="row pad-more">
+
 		<div class="col-lg-2 col-md-2 col-sm-3 hidden-xs left-bar">
+		</div>
 		
+		<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 content">
 			<?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); ?>
 				<?php $pid = get_the_ID(); ?>
-				
 
-				<img src="<?php  echo $r = get_avatar_url( get_the_author_meta( 'ID' )); ?>" class="img-circle img-responsive profileimgtwo">
-				<p><?php the_author_firstname(); echo' '; the_author_lastname(); ?>
-				</p><p>
-
-				
-					<?php 
-					$date = date('h:i A jS F Y',get_post_time('U', true));
-
-					echo $r =  time_elapsed_string($date); ?> </p>
-
-					
-
-						<?php $category = get_the_category();
-						foreach ($category as $val) {?>
-
-							<p><span class="ashtwo">
-
-							<?php   echo $val->cat_name; ?>
-
-							</span></p>
-						<?php }
-
-						?>
-						
-
-					
-
-				</div>
-				<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 content">
-
-					<p class="title"><?php the_title();?></p>
+				<p class="title"><?php the_title();?></p>
 
 
 
-					<?php if ( has_post_thumbnail($post) ) {
+				<?php if ( has_post_thumbnail($post) ) {
 
-						$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
-						<img src="<?php echo $url; ?>" longdesc="<?php the_title(); ?>" alt="<?php the_title(); ?>" class="img-responsive" />
+					$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+					<img src="<?php echo $url; ?>" longdesc="<?php the_title(); ?>" alt="<?php the_title(); ?>" class="img-responsive" />
 
-						<?php }?>
+					<?php }?>
 
-						<p class="body">
+					<p class="body">
 
 						<?php $c = get_the_content();
 
 						echo $c;
 						?>
 
-						</p>
+					</p>
+
+					<div class="row meta-post">
+						<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+
+							<img src="<?php  echo $r = get_avatar_url( get_the_author_meta( 'ID' )); ?>" class="img-responsive profileimgtwo">
+
+							<p class="nom"><?php the_author_firstname(); echo' '; the_author_lastname(); ?>
+							</p><p class="nom">
 
 
-							<div class="row prev-next-links">
-								<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 prev">
+							<?php 
+							$date = date('h:i A jS F Y',get_post_time('U', true));
+
+							echo $r =  time_elapsed_string($date); ?> </p>
+
+
+							<p>
+								<?php $category = get_the_category();
+								foreach ($category as $val) {?>
+
+								<span class="ashtwo">
+
+									<?php   echo $val->cat_name; ?>
+
+								</span>
+								<?php }
+
+
+								$posttags = get_the_tags();
+								if ($posttags) {
+
+
+									foreach($posttags as $tag) { 
+										?>
+										<span class="ashtwo tag">
+											<?php echo $tag->name . ' '; ?>
+										</span>
+
+
+										<?php }
+
+
+
+
+									}
+									?>
+
+								</p>
+							</div>
+
+
+
+						</div>
+
+
+						<div class="row prev-next-links">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 prev">
 								<p>
 							<!-- <a href="">
 
@@ -83,7 +108,11 @@
 						</p></div>
 					</div>
 
-					<div class="row">
+					<div class="row before-thumb-more-row">
+					<h4> You might Like: </h4>
+					</div>
+
+					<div class="row thumb-more-row">
 
 
 
@@ -97,31 +126,54 @@
 							?>
 
 
-						<div class="col-lg-2 col-md-2 col-sm-4  hidden-xs thumb-more <?php if (!has_post_thumbnail($recent['ID']) ){ echo 'pad-little';
-							} ?> ">
-							<a href="<?php echo get_permalink($recent["ID"]); ?>">
+							<div class="col-lg-3 col-md-3 col-sm-3  hidden-xs thumb-more <?php if (!has_post_thumbnail($recent['ID']) ){ echo 'pad-little';
+						} ?> ">
+						<a href="<?php echo get_permalink($recent["ID"]); ?>">
 
-<?php if ( has_post_thumbnail($recent['ID']) ) {
-	
-
-										$url = wp_get_attachment_url( get_post_thumbnail_id($recent["ID"]) ); 
-										?>
-										<img src="<?php echo $url; ?>" longdesc="<?php the_title(); ?>" alt="<?php the_title(); ?>" class="img-responsive" />
-
-										<?php }
-										?>
-											 
-
-										
+							<?php if ( has_post_thumbnail($recent['ID']) ) {
 
 
-								<p><?php echo $recent["post_title"]; ?></p>
-								<?php if (!has_post_thumbnail($recent['ID']) ) { ?>
+								$url = wp_get_attachment_url( get_post_thumbnail_id($recent["ID"]) ); 
+								?>
 
-								<span></span>
+
+
+
+
+
+
+								<img src="<?php echo $url; ?>" longdesc="<?php the_title(); ?>" alt="<?php the_title(); ?>" class="img-responsive img-circle" />
+
+								<?php }
+								?>
+
+								<?php if (!has_post_thumbnail($recent['ID']) ) { 
+													 $sub_title = "";
+													 $title = $recent["post_title"];
+													 $title_array = explode(" ", $title);
+
+													 foreach ($title_array as $value) {
+													 	$sub_title .= substr($value, 0,1);
+													 	
+													 }
+													 $sub_title = substr($sub_title, 0,4);
+
+													
+
+
+									?>
+
+         							<div class="circle"><?php echo $sub_title; ?> </div>
 
 
 								<?php } ?>
+
+
+
+
+
+								<p><?php echo $recent["post_title"]; ?></p>
+								
 
 
 							</a>
@@ -135,23 +187,23 @@
 
 
 					</div>
-				<?php comments_template( '/short-comments.php' ); ?>
-
-
-			
+					<?php comments_template( '/short-comments.php' ); ?>
 
 
 
 
 
 
-			</div>
 
-		<?php endwhile; ?>
-	<?php endif; ?>
-	<div class="col-lg-1 col-md-2 hidden-sm hidden-xs"></div>
 
-</div>
+
+				</div>
+
+			<?php endwhile; ?>
+		<?php endif; ?>
+		<div class="col-lg-1 col-md-2 hidden-sm hidden-xs"></div>
+
+	</div>
 
 
 </div>
