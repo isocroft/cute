e87fe704ce8e8e97081bab8276e81647ff686944
lyndachiwetelu <lyndachiwe@gmail.com>
2016-02-24 15:04:35 +0100

    Fixes

--- single.php
+++ single.php
@@ -215,7 +215,7 @@
 							<div class="row">
 								<div class="col-lg-2 col-md-2 col-sm-1 hidden-xs left-bar">
 								</div>
-								<div class="col-lg-9 col-md-5 col-sm-10 col-xs-10">
+								<div class="col-lg-7 col-md-7 col-sm-10 col-xs-10">
 								<?php comments_template( '/comments.php' ); ?>
 								</div>
 
